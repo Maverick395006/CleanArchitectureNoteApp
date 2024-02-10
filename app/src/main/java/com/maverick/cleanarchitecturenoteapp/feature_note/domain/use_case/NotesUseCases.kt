10591f9e1c95b6f290ec5a23c301a51deeaa4329
Piyush <piyush.radadiya288@gmail.com>
2024-02-10 18:52:29 +0530

    NoteUseCases wrapper class

---
+++ app/src/main/java/com/maverick/cleanarchitecturenoteapp/feature_note/domain/use_case/NotesUseCases.kt
@@ -0,0 +1,6 @@
+package com.maverick.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+data class NotesUseCases(
+    val getNotes: GetNotesUseCase,
+    val deleteNote: DeleteNoteUseCase,
+)
