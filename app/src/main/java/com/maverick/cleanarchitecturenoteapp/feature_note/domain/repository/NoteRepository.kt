50e3ebabe9fc9c116b324a954b59f918adb6f0ae
Piyush <piyush.radadiya288@gmail.com>
2024-02-10 15:01:31 +0530

    Setting up Note Repository: - Room DB (Database) - Repository(interface in domain & implementation in data layer)

---
+++ app/src/main/java/com/maverick/cleanarchitecturenoteapp/feature_note/domain/repository/NoteRepository.kt
@@ -0,0 +1,16 @@
+package com.maverick.cleanarchitecturenoteapp.feature_note.domain.repository
+
+import com.maverick.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+interface NoteRepository {
+
+    fun getAllNotes(): Flow<List<Note>>
+
+    suspend fun getNoteById(id: Int): Note?
+
+    suspend fun insertNote(note: Note)
+
+    suspend fun deleteNote(note: Note)
+
+}
